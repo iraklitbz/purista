dccf824e8182272adc90d324789acb9630f63c5b
irakli Tavberidze <irakli.tavberidze@tuispain.com>
2022-12-13 19:47:25 +0100

    color change

--- components/Footer.vue
+++ components/Footer.vue
@@ -6,11 +6,19 @@
             </nuxt-link>
         </div>
          <ul class="flex justify-center">
-            <li class="text-center text-white text-sm mr-2"><a cl target="_blank" href="https://www.facebook.com/profile.php?id=100064998761253"><load-svg name="facebook" class="text-primary hover:text-white w-10" /></a></li>
-            <li class="text-center text-white text-sm"><a target="_blank" href="https://www.instagram.com/purista.ge/"><load-svg name="instagram" class="text-primary hover:text-white w-10" /></a></li>
+            <li class="text-center text-white text-sm mr-2"><a cl target="_blank" href="https://www.facebook.com/profile.php?id=100064998761253"><load-svg name="facebook" class="text-facebook hover:text-white w-10" /></a></li>
+            <li class="text-center text-white text-sm"><a target="_blank" href="https://www.instagram.com/purista.ge/"><load-svg name="instagram" class="text-instagram hover:text-white w-10" /></a></li>
         </ul>
         <div class="max-w-4xl m-auto w-full mt-10 flex justify-center border-solid border-t-2 py-2 border-primary">
             <p class="text-center text-sm text-white">{{ $t('footer.parraf') }}</p>
         </div>
     </footer>
 </template>
+<style>
+    .text-instagram {
+       color: #da2e69;
+    }
+    .text-facebook {
+       color: #1A6ED8;
+    }
+</style>
